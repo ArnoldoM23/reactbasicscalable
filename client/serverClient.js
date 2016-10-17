48b2cf3f5011148cde5c7a24573b4f745c4965ec
AnorldoM23 <arnoldomunoz23@yahoo.com>
2016-10-17 16:14:27 -0700

    first commit

--- client/serverClient.js
+++ client/serverClient.js
@@ -6,20 +6,16 @@ const app = express();
 const port = process.env.PORT || 3000;
 const cors = require('cors');
 var path = require('path');
-const config = require('./webpack.pro.config')
-const webpack = require('webpack');
 
 var publicPath = path.resolve(__dirname, 'client');
 
-const compiler = webpack(config);
+
 
 app.use(morgan('combined'));
 app.use(bodyParser.json());
 app.use(express.static(publicPath));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, './src/index.html'));
-});
+
 
 const server = http.createServer(app);
 
